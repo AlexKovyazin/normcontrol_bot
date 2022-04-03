06c5b412f93b49d7d9f25ada59c48946014950f9
AlexKovyazin <KovyazinAlexander@yandex.ru>
2022-04-03 19:34:28 +0300

    refactor: refactor for webhooks

--- bot.py
+++ bot.py
@@ -1,13 +1,31 @@
+import os
 import telebot
+from flask import Flask, request
+from config import TOKEN, URL
 
 
-TOKEN = '2108392950:AAHNePPNGmIbDuLuGPTUHuJKBdFDeWXZfdQ'
 SHOW_STAT_COMMAND = 'НОРМОКОНТРОЛЬ, ЁБАНА, НУЖНА СТАТИСТИКА'
 
 bot = telebot.TeleBot(TOKEN)
+server = Flask(__name__)
 
 # Глобальный счетчик сообщений
 counter = {}
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=URL + TOKEN)
+    return "!", 200
+
+
+@server.route('/' + TOKEN, methods=['POST'])
+def get_message():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
 
 
 @bot.message_handler(content_types=['audio', 'photo', 'voice', 'video', 'document',
@@ -50,5 +68,5 @@ def show_stat(message):
     bot.send_message(message.chat.id, stat)
 
 
-# Run
-bot.infinity_polling(timeout=10, long_polling_timeout=5)
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
--- config.py
+++ config.py
@@ -1,1 +1,8 @@
 TOKEN = '2108392950:AAHNePPNGmIbDuLuGPTUHuJKBdFDeWXZfdQ'
+URL = 'https://normcontrolbot.herokuapp.com/'
+HOST = 'ec2-63-35-156-160.eu-west-1.compute.amazonaws.com'
+DATABASE = 'd6m2902kvhirdq'
+USER = 'uhsyrrueybiiph'
+PORT = 5432
+PASSWORD = '501052069e716f618e439df4e8c27319f347e8dc00dd10e0c2da901c2d2976e1'
+URI = 'postgres://uhsyrrueybiiph:501052069e716f618e439df4e8c27319f347e8dc00dd10e0c2da901c2d2976e1@ec2-63-35-156-160.eu-west-1.compute.amazonaws.com:5432/d6m2902kvhirdq'
